40ad3527675d1182d45c0b2094740b47df1d4cbe
vaseba <badasin.valentin@yandex.ru>
2021-03-21 20:59:31 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// 1 Переменные
+var name = 'Valentin' //лучше не использовать
+const lastName = 'Badasin' //неизменяемая
+const firstName = 'Valentin' //неизменяемая
+// const age = 33
+const isProgrammer = true
+
+
+// 2 Мутирование
+// console.log('Имя: ' + firstName + ', а возраст: ' + age)
+// alert('Имя: ' + firstName + ', а возраст: ' + age)
+
+// const nameC = prompt('Введите Фамилию')
+// alert(firstName + ' ' + nameC)
+
+
+// 3 Операторы
+let currentYear = 2021
+const birthYear = 1987
+const a = 10
+const b = 5
+
+const age = currentYear - birthYear
+
+console.log(currentYear++)
+console.log(currentYear++)
